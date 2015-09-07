5e760e34f0e0f96e151343f850579d43c74debd6
Proudmoor <zpf238@gmail.com>
2015-09-08 00:41:32 +0800

    merge

---
+++ ExG4ActionInitialization01.hh
@@ -0,0 +1,14 @@
+//
+//  ExG4ActionInitialization01.h
+//  Geant4Test
+//
+//  Created by zpf on 15-9-7.
+//
+//
+
+#ifndef __Geant4Test__ExG4ActionInitialization01__
+#define __Geant4Test__ExG4ActionInitialization01__
+
+#include <stdio.h>
+
+#endif /* defined(__Geant4Test__ExG4ActionInitialization01__) */
---
+++ ExG4DetectorConstruction01.hh
@@ -0,0 +1,17 @@
+//
+//  ExG4DetectorConstruction.h
+//  Geant4Test
+//
+//  Created by zpf on 15-9-7.
+//
+//
+
+#ifndef __Geant4Test__ExG4DetectorConstruction__
+#define __Geant4Test__ExG4DetectorConstruction__
+
+#include <stdio.h>
+
+class ExG4DetectorConstruction01:public G4UserDetectorConstruction{
+    
+};
+#endif /* defined(__Geant4Test__ExG4DetectorConstruction__) */
